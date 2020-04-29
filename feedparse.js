d691ea66618cc4c90e29b8d63c893533961c7574
anubhav77 <anubhav77@gmail.com>
2020-04-29 16:57:22 +0530

    Rename project

--- feedparse.js
+++ feedparse.js
@@ -9,7 +9,7 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, n);
         this.urls = n.urls;
         if (n.interval > 35790) {
-            this.warn(RED._("custom-feedparser.errors.invalidinterval"))
+            this.warn(RED._("multifeed-parser.errors.invalidinterval"))
         }
         this.interval = (parseInt(n.interval) || 15) * 60000;
         var node = this;
@@ -20,7 +20,7 @@ module.exports = function (RED) {
             feed_urls.forEach(function (feed_url, index) {
                 var parsedUrl = url.parse(feed_url);
                 if (!(parsedUrl.host || (parsedUrl.hostname && parsedUrl.port)) && !parsedUrl.isUnix) {
-                    this.error(RED._("custom-feedparser.errors.invalidurl" + feed_url));
+                    this.error(RED._("multifeed-parser.errors.invalidurl" + feed_url));
                 } else {
                     var req = request(feed_url, {timeout: 10000, pool: false});
                     //req.setMaxListeners(50);
@@ -35,7 +35,7 @@ module.exports = function (RED) {
 
                     req.on('response', function (res) {
                         if (res.statusCode != 200) {
-                            node.warn(RED._("custom-feedparser.errors.badstatuscode") + " " + res.statusCode);
+                            node.warn(RED._("multifeed-parser.errors.badstatuscode") + " " + res.statusCode);
                         } else {
                             res.pipe(feedparser);
                         }
@@ -74,5 +74,5 @@ module.exports = function (RED) {
         getFeed();
     }
 
-    RED.nodes.registerType("custom-feedparser", FeedParseNode);
+    RED.nodes.registerType("multifeed-parser", FeedParseNode);
 };
