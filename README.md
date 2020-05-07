ad4a43e88e6eb025ad8094c0ca455679dc62ffee
anubhav77 <anubhav77@gmail.com>
2020-05-07 21:02:37 +0530

    Accept msg.feedUrls as input

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ node-red-contrib-multifeed-parser
 
 A modified version of [node-red-node-feedparser-extended](https://github.com/arossmann/node-red-contrib-feedparser-extended) 
 
-However, Functionalty is more closely aligned with base node-red feedparser. This accepts a comma separated list of feed-urls.
+However, Functionalty is more closely aligned with base node-red feedparser. This node accepts a comma separated list of feed-urls in Node configuration.
+
+This node also accepts msg.feedUrls as input . An input without feedUrls, will simply force a refresh of feeds and reset of refresh timer.
 
 Impact on performance or limit on how many feeds it can support is not tested.
  
--- feedparse.js
+++ feedparse.js
@@ -7,7 +7,7 @@ module.exports = function (RED) {
 
     function FeedParseNode(n) {
         RED.nodes.createNode(this, n);
-        this.urls = n.urls;
+        this.urls = n.urls||"";
         if (n.interval > 35790) {
             this.warn(RED._("multifeed-parser.errors.invalidinterval"))
         }
@@ -16,11 +16,13 @@ module.exports = function (RED) {
         this.interval_id = null;
         this.seen = {};
         var getFeed = function () {
+            node.log("Getfeed fired");
             var feed_urls = node.urls.split(",");
             feed_urls.forEach(function (feed_url, index) {
+                node.log("feed url is::"+feed_url)
                 var parsedUrl = url.parse(feed_url);
                 if (!(parsedUrl.host || (parsedUrl.hostname && parsedUrl.port)) && !parsedUrl.isUnix) {
-                    this.error(RED._("multifeed-parser.errors.invalidurl" + feed_url));
+                    node.error(RED._("multifeed-parser.errors.invalidurl" + feed_url));
                 } else {
                     var req = request(feed_url, {timeout: 10000, pool: false});
                     //req.setMaxListeners(50);
@@ -72,6 +74,21 @@ module.exports = function (RED) {
             getFeed();
         }, node.interval);
         getFeed();
+        this.on("input",function(msg){
+            node.log("Got a new input:"+msg);
+            if(null !== msg && null !== msg.feedUrls){
+                this.urls=msg.feedUrls;
+                getFeed();
+            }
+            if (null !== this.interval_id){
+                clearInterval(this.interval_id);
+                this.interval_id=setInterval(function () {
+                    getFeed();
+                }, this.interval);
+            }
+
+
+        });
     }
 
     RED.nodes.registerType("multifeed-parser", FeedParseNode);
